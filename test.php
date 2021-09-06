468fbd199e3930f4b3d6529234f6a74d9926a333
shawes <shawes@waylandgames.co.uk>
2021-09-06 13:33:17 +0100

    Added better error logging in terminal

--- test.php
+++ test.php
@@ -5,6 +5,7 @@ require "Classes\search.class.php";
 $file_path = "input\input.csv";
 $output_filename = "output\output.csv";
 $err_count = 0;
+$err_lines = "";
 $output = "";
 
 if (($handle = fopen($file_path, "r")) !== FALSE) {
@@ -18,7 +19,10 @@ if (($handle = fopen($file_path, "r")) !== FALSE) {
                 try {
                     $gameObj->downloadBoxArt();
                 } catch (BadFunctionCallException $e) { $err_count++;}
-            } catch (BadFunctionCallException $e) {$err_count++;}
+            } catch (BadFunctionCallException $e) {
+                $err_count++;
+                $err_lines .= " - $game\n";
+            }
         }
     }
     fclose($handle);
@@ -28,5 +32,6 @@ file_put_contents($output_filename, $output);
 
 
 if ($err_count > 0) {
-    echo "Number of errors: $err_count";
+    echo "\nNumber of errors: $err_count\n";
+    echo "Erroring lines:\n$err_lines";
 }
